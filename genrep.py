353a17a941247d573b8e09709c50d15d05be9711
siem-gaber <116160960+siem-gaber@users.noreply.github.com>
2023-10-11 14:57:30 +0200

    commit message

---
+++ genrep.py
@@ -0,0 +1,24 @@
+def main():
+
+   print("Beräknar differensen av jämna/udda tal...")
+
+
+   sum_odd = 0
+   sum_even = 0
+
+
+   for i in range(1, 1000, 2):
+       sum_odd += i
+
+
+   for i in range(2, 1000, 2):
+       sum_even += i
+
+ 
+   difference = sum_odd - sum_even
+
+
+   print("Differensen=", difference)
+
+
+
